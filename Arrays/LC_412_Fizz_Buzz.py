5bf07f18eb2c9abb5de4710eb6700512d24629f4
azfaryasin <azfaryasinpersonal@gmail.com>
2026-06-09 14:06:02 +0530

    solved: LC-412 Fizz Buzz problem

---
+++ Arrays/LC_412_Fizz_Buzz.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        b = []
+        for i in range(1,n+1):
+            if i % 3 == 0 and i % 5 == 0 :
+                b.append("FizzBuzz")
+            elif i % 3 == 0 : 
+                b.append("Fizz")
+            elif i % 5 == 0 :
+                b.append("Buzz")
+            else:
+                b.append(str(i))
+        return b 
+            
