67dbe312536254680a7b0a250197e7e50d838bf7
azfaryasin <azfaryasinpersonal@gmail.com>
2026-06-09 14:13:02 +0530

    solved: LC-2239 Find closest to Zero problem

---
+++ Arrays/LC_2239_Closest_To_Zero.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findClosestNumber(self, nums):
+        closest = nums[0]
+        for i in nums:
+            if abs(i) == abs(closest):
+                closest = max(closest,i)
+            elif abs(i) < abs(closest):
+                closest = i
+        
+        return closest
