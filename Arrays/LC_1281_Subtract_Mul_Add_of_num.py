d0924503e784acd158e11e8aafd56f8cec4435ae
azfaryasin <azfaryasinpersonal@gmail.com>
2026-06-14 22:42:53 +0530

    solved: LC-1281 Subtract Multiplication Addition of Integer problem

---
+++ Arrays/LC_1281_Subtract_Mul_Add_of_num.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def subtractProductAndSum(self, n):
+        x = n
+        res = 0
+        s=0
+        m=1
+        while x>0:
+            res = x%10
+            s+=res
+            m*=res
+            x=x//10
+        return m-s
