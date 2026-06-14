a01bde1c1a0c1d6fee8fc7796e19c43c279d413b
azfaryasin <azfaryasinpersonal@gmail.com>
2026-06-14 22:53:20 +0530

    solved: LC-191 Number of 1 Bits problem

---
+++ Arrays/LC_191_Number_of_1Bits.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        num = bin(n)
+        res =0
+        for i in num:
+            if i == "1":
+                res+=1
+            else:
+                continue
+        return res
