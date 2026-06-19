1d503a6d011b5b822ae751e445daff866e4036c9
azfaryasin <azfaryasinpersonal@gmail.com>
2026-06-19 23:57:34 +0530

    LC-09 Second approach added

--- Arrays/LC_09_Palindrome_Num.py
+++ Arrays/LC_09_Palindrome_Num.py
@@ -8,3 +8,12 @@ class Solution(object):
             res = res*10+x%10
             x = x//10
         return res == num
+
+# Second Approach
+class Solution(object):
+    def isPalindrome(self, x):
+        if x <0:
+            return False
+        num = x
+        res = int(str(x)[::-1])
+        return res == num
