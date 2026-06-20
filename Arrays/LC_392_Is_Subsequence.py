2712d0bd18f9a18aa223c3a779dd3ffc1d5dd74d
azfaryasin <azfaryasinpersonal@gmail.com>
2026-06-20 23:29:33 +0530

    LC-392 Second approach added

--- Arrays/LC_392_Is_Subsequence.py
+++ Arrays/LC_392_Is_Subsequence.py
@@ -7,3 +7,20 @@ class Solution(object):
                 i += 1
             j += 1
         return i == len(s)
+
+#Second Approach
+class Solution(object):
+    def isSubsequence(self, s, t):
+        a = 0
+        n = len(s)
+        b=0
+        if len(s) == 0:
+            return True
+
+        for i in range(0,len(t)):
+            if a < n and s[a] == t[i]:
+                a +=1 
+            if a == n:
+                return True
+        else:
+            return False
